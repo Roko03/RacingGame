d848eedaceac168bc8d3e5007221b17de0533242
Roko <rokoponjarac2@gmail.com>
2025-05-17 10:34:58 +0200

    Make Countdown manager

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    public TextMeshProUGUI CountDown;
+    public AudioSource GetReady;
+    public AudioSource GoAudio;
+    public GameObject LapTimer;
+    public GameObject CarControls;
+
+
+    void Start()
+    {
+        CarControls.SetActive(false); 
+        StartCoroutine(CountStart());
+    }
+
+    IEnumerator CountStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        CountDown.text = "3";
+        GetReady.Play();
+        CountDown.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1);
+
+        CountDown.gameObject.SetActive(false);
+        CountDown.text = "2";
+        GetReady.Play();
+        CountDown.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1);
+
+        CountDown.gameObject.SetActive(false);
+        CountDown.text = "1";
+        GetReady.Play();
+        CountDown.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1);
+
+        CountDown.gameObject.SetActive(false);
+        GoAudio.Play();
+        LapTimer.SetActive(true);
+        CarControls.SetActive(true);
+    }
+}
