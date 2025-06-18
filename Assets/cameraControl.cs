34865207e9f432f62b433e13d47041bcf9564b1a
Roko <rokoponjarac2@gmail.com>
2025-06-18 18:45:42 +0200

    Import race car prefab

---
+++ Assets/cameraControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraSwitch : MonoBehaviour
+{
+   
+    public Unity.Cinemachine.CinemachineCamera thirdPersonCam;
+    public Unity.Cinemachine.CinemachineCamera hoodCam;
+
+    private void Start()
+    {
+        SetThirdPersonView();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (thirdPersonCam.Priority > hoodCam.Priority)
+            {
+                SetHoodView();
+            }
+            else
+            {
+                SetThirdPersonView();
+            }
+        }
+    }
+
+    private void SetThirdPersonView()
+    {
+        thirdPersonCam.Priority = 10;
+        hoodCam.Priority = 5;
+    }
+
+    private void SetHoodView()
+    {
+        thirdPersonCam.Priority = 5;
+        hoodCam.Priority = 10;
+    }
+}
